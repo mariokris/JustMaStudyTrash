f6dfd3b61fe3cd2ed50f18d705fb8ce85a14829c
mariokris <46968477+mariokris@users.noreply.github.com>
2019-02-03 23:06:00 +0700

    Create NaikTurunPenumpang.js
    Latihan Array dan Function

---
+++ NaikTurunPenumpang.js
@@ -0,0 +1,55 @@
+<html>
+<head>
+	<TITLE>Mulai FUNGSI</TITLE>
+	<style type="text/css">
+		#penumpang_naik{
+			position: relative;
+			left: 10px;
+			border-radius: 5px;
+			border : solid black 1px;
+			background: none;
+			outline: none;
+		}
+		#penumpang_turun{
+			border-radius: 5px;
+			border : solid black 1px;
+			background: none;
+			outline: none;
+		}
+		#PENUMPANG{
+			width: 500px;
+			height: 100px;
+			resize: none;
+		}
+	</style>
+</head>
+<body>
+	<h1 style="color:blue" align="center">BELAJAR MARIO</h1>
+	<br><br>
+	ISI PENUMPANG DALAM ANGKOT:
+	<br><br>
+	<textarea id='PENUMPANG'></textarea>
+	<br>
+	<button id='penumpang_turun'>PENUMPANG TURUN</button>
+	<button id='penumpang_naik'>PENUMPANG NAIK</button>
+	<script type="text/javascript" src="latihanfungsidanarray.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+	<script type="text/javascript">
+		$(document).ready(function(){
+			$('#penumpang_naik').click(function(){
+				var nama = prompt('Masukkan Nama');
+				tambahpenumpang(nama,penumpang);
+				$('#PENUMPANG').val(penumpang.join());
+				console.log(penumpang);
+			})
+			$('#penumpang_turun').click(function(){
+				var nama = prompt('Masukkan Nama');
+				hapuspenumpang(nama,penumpang);
+				$('#PENUMPANG').val(penumpang.join());
+				console.log(penumpang);
+			})
+			
+		})
+	</script>
+</body>
+</html>
